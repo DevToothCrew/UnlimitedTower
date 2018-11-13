ad40170116580a9128551ffee9535e49ee1c872b
kyle <kyle@devtooth.com>
2018-11-13 14:45:53 +0900

    Test Commit

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/TestScript/TestResultButton.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/TestScript/TestResultButton.cs
@@ -21,7 +21,7 @@ public class TestResultButton : MonoBehaviour
     }
     private void OnGUI()
     {
-        if (BattleManager.Inst.BattleState == BATTLE_STATE.END)
+        //if (BattleManager.Inst.BattleState == BATTLE_STATE.END)
         {
             if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 100, 70), "GameOver"))
             {
