2aad6cb8ab7dc4d013501fb6660d767b13789fd6
kyle <kyle@devtooth.com>
2018-11-20 17:41:42 +0900

    Gacha Background Update_1

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/TestScript/TestResultButton.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/TestScript/TestResultButton.cs
@@ -10,6 +10,7 @@ public class TestResultButton : MonoBehaviour
 
     private void OnGUI()
     {
+        // 배틀 끝났을 때 뜨는 버튼.
         //if (BattleManager.Inst.BattleState == BATTLE_STATE.END)
         {
             if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 100, 70), "GameOver"))
