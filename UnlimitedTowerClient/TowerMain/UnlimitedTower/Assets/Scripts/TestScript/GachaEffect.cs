a6f4f853b57f6b5a907b1f5422d32707c4ef8d2d
kyle <kyle@devtooth.com>
2018-11-22 19:30:10 +0900

    Gacha Ui Fix_2

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/TestScript/GachaEffect.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/TestScript/GachaEffect.cs
@@ -6,53 +6,24 @@ public class GachaEffect : MonoBehaviour {
 
     public Animator fadeOut;
 
-    public Animator lightEffectCircle01;
-    public Animator lightEffectCircle02;
-    public Animator lightEffectCircle03;
-    public Animator lightEffectCircle04;
+    public Animator lightEffectCircle01Animator;
+    public Animator lightEffectCircle02Animator;
+    public Animator lightEffectCircle03Animator;
+    public Animator lightEffectCircle04Animator;
 
+    public Animator purpleCircleAnimator;
     public Animator blackHoleAnimator;
 
     public GameObject blackHole;
     public GameObject lightEffectCircle;
 
-    public bool MouseClick = false;
 
     private void Awake()
     {
         blackHole.GetComponent<RectTransform>().localEulerAngles = new Vector3(0.0f, 0.0f, 180.0f);
     }
  
-    private void OnEnable()
-    {
-        // 애니메이션 기본값 설정.
-        MouseClick = false;
-        lightEffectCircle01.SetBool("Play", false);
-        lightEffectCircle02.SetBool("Play", false);
-        lightEffectCircle03.SetBool("Play", false);
-        lightEffectCircle04.SetBool("Play", false);
 
-        //for(int i=0; i<4; i++)
-        //{
-        //    if (lightEffectCircle.transform.GetChild(i).GetComponent<RectTransform>())
-        //    {
-        //        Debug.Log("LightEffect : " + lightEffectCircle.transform.GetChild(i).name);
-        //        lightEffectCircle.transform.GetChild(i).GetComponent<RectTransform>().localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        //    }
-          
-        //}    
-        if (blackHole.GetComponent<RectTransform>())
-        {
-
-            Debug.Log("블랙홀 각도 : " + blackHole.GetComponent<RectTransform>().eulerAngles.ToString());
-            Debug.Log("블랙홀 각도 : " + blackHole.GetComponent<RectTransform>().eulerAngles.ToString());
-
-           // blackHole.GetComponent<RectTransform>().localEulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
-
-        }
-
-
-    }
 
     IEnumerator FADE_OUT()
     {
@@ -64,7 +35,6 @@ public class GachaEffect : MonoBehaviour {
      fadeOut.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f);
 
 
-        //lightEffectCircle01.SetBool("Play", false);
         fadeOut.SetBool("Play", false);
         yield break;
     }
@@ -75,11 +45,9 @@ public class GachaEffect : MonoBehaviour {
         {
             yield return null;
         }
-        while (lightEffectCircle01.GetCurrentAnimatorStateInfo(0).IsName("Wave") &&
-      lightEffectCircle01.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f);
+        while (lightEffectCircle01Animator.GetCurrentAnimatorStateInfo(0).IsName("Wave") &&
+      lightEffectCircle01Animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f);
 
-
-        //lightEffectCircle01.SetBool("Play", false);
 
         fadeOut.SetBool("Play", true);
         yield return StartCoroutine("FADE_OUT");
@@ -91,15 +59,15 @@ public class GachaEffect : MonoBehaviour {
         {
             yield return null;
         }
-        while (lightEffectCircle02.GetCurrentAnimatorStateInfo(0).IsName("Wave") &&
-      lightEffectCircle02.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f);
+        while (lightEffectCircle02Animator.GetCurrentAnimatorStateInfo(0).IsName("Wave") &&
+      lightEffectCircle02Animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f);
 
 
         //lightEffectCircle02.SetBool("Play", false);
 
 
         //yield break;
-        lightEffectCircle01.SetBool("Play", true);
+        lightEffectCircle01Animator.SetBool("Play", true);
         yield return StartCoroutine("WAVE_LIGHT_EFFECT_CIRCLE01");
     }
     IEnumerator WAVE_LIGHT_EFFECT_CIRCLE03()
@@ -108,14 +76,14 @@ public class GachaEffect : MonoBehaviour {
         {
             yield return null;
         }
-        while (lightEffectCircle03.GetCurrentAnimatorStateInfo(0).IsName("Wave") &&
-      lightEffectCircle03.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f);
+        while (lightEffectCircle03Animator.GetCurrentAnimatorStateInfo(0).IsName("Wave") &&
+      lightEffectCircle03Animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f);
 
 
-    
+
 
         //yield break;
-         lightEffectCircle02.SetBool("Play", true);
+        lightEffectCircle02Animator.SetBool("Play", true);
          yield return StartCoroutine("WAVE_LIGHT_EFFECT_CIRCLE02");
     }
     IEnumerator WAVE_LIGHT_EFFECT_CIRCLE04()
@@ -124,10 +92,10 @@ public class GachaEffect : MonoBehaviour {
         {
             yield return null;
         }
-        while (lightEffectCircle04.GetCurrentAnimatorStateInfo(0).IsName("Wave") &&
-      lightEffectCircle04.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f);
+        while (lightEffectCircle04Animator.GetCurrentAnimatorStateInfo(0).IsName("Wave") &&
+      lightEffectCircle04Animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f);
 
-        lightEffectCircle03.SetBool("Play", true);
+        lightEffectCircle03Animator.SetBool("Play", true);
         yield return StartCoroutine("WAVE_LIGHT_EFFECT_CIRCLE03");
     }
 
@@ -135,12 +103,25 @@ public class GachaEffect : MonoBehaviour {
 
     public void GoGacha()
     {
-        
-            MouseClick = true;
-            lightEffectCircle04.SetBool("Play", true);
-            blackHoleAnimator.SetBool("Play", true);
-            StartCoroutine("WAVE_LIGHT_EFFECT_CIRCLE04");
-        
+
+        lightEffectCircle04Animator.SetBool("Play", true);
+        blackHoleAnimator.SetBool("Play", true);
+        purpleCircleAnimator.SetBool("Play", true);
+        StartCoroutine("WAVE_LIGHT_EFFECT_CIRCLE04");
+
+    }
+
+    public void ShowGachaResult()
+    {
+        //PacketManager.Inst.Request_Gacha();
+        Debug.Log("Gacha Result");
+
+        Character newChar = new Character(UserDataManager.Inst.GetCharacterIndex() + 1);
+        UserDataManager.Inst.SetCharacter(newChar);
+
+        UserDataManager.Inst.AddNewChar(newChar.Name);
+
+        // var instance = Instantiate(Resources.Load("Prefabs/CharElement") as GameObject);
     }
 	
 	
