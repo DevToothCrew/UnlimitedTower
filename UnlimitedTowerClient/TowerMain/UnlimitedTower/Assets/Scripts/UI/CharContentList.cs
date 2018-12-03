c36fa072b1710d7ac1a86dec8bd5f747ab44e8cf
kyle <kyle@devtooth.com>
2018-12-03 16:04:30 +0900

    Replace Character Image_3

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/UI/CharContentList.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/UI/CharContentList.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharContentList : MonoBehaviour {
+public class CharContentList : MonoSingleton<CharContentList>
+{
 
 	// Use this for initialization
 	void Awake () {
        UserDataManager.Inst.LoadCharList();
     }
+    void LoadCharContentList()
+    {
+
+    }
+
 	
 }
