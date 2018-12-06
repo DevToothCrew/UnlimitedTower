69eb0d916e7807ed04a1c21649f8d5176c09fa60
kyle <kyle@devtooth.com>
2018-12-06 19:30:59 +0900

    Reorder Charactere_1

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/UI/FormationDeck.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/UI/FormationDeck.cs
@@ -44,7 +44,7 @@ public class FormationDeck : MonoBehaviour, IPointerClickHandler
 
         Debug.Log("formatonDeck : " + DeckNum);
         int charIndex = UserDataManager.Inst.formationDic[DeckNum];
-        UserDataManager.Inst.formationDic.Remove(DeckNum);
+        //UserDataManager.Inst.formationDic.Remove(DeckNum);
         if(DeckNum<5)
         {
             UserDataManager.Inst.monsterDic[charIndex].OnFormation = false;
@@ -52,15 +52,95 @@ public class FormationDeck : MonoBehaviour, IPointerClickHandler
         }
         else
         {
-            UserDataManager.Inst.servantDic[charIndex].OnFormation = false;
-            UserDataManager.Inst.servantDic[charIndex].FormationIndex = -1;
+            //UserDataManager.Inst.servantDic[charIndex].OnFormation = false;
+            //UserDataManager.Inst.servantDic[charIndex].FormationIndex = -1;
+            Reorder(ref UserDataManager.Inst.servantDic, 0);
         }
 
 
      
-        ShowEmptyText(true);
+        //ShowEmptyText(true);
 
     }
+
+    // 5 6 7 8 9
+    // 순서 7 -> 6  -> 8 -> 5 -> 9
+
+    void Reorder(ref Dictionary<int, Character> charDic, int charType)
+    {
+       GameObject FormationList =  LobbyManager.Inst.FormationList.gameObject;
+        int index = -1;
+
+
+
+        for(int i=0; i<UserDataManager.Inst.formationOrderList.Count; i++)
+        {
+            if(UserDataManager.Inst.formationOrderList[i] == DeckNum)
+            {
+                index = i;
+                break;
+            }
+        }
+
+
+        // 덱 삭제
+        int charIndex = UserDataManager.Inst.formationDic[DeckNum];
+
+
+
+        UserDataManager.Inst.formationDic.Remove(DeckNum);
+        charDic[charIndex].OnFormation = false;
+        charDic[charIndex].FormationIndex = -1;
+        // empty 활성화
+
+        int preDeckNum = UserDataManager.Inst.formationOrderList[index];
+        int nextDeckNum = UserDataManager.Inst.formationOrderList[++index];
+
+    
+
+
+        int nextCharDicKey = UserDataManager.Inst.formationDic[nextDeckNum];
+
+        while (nextDeckNum != 2)
+        {
+
+            FormationList.transform.GetChild(preDeckNum).gameObject.transform.GetChild(0).GetComponent<Image>().sprite =
+          FormationList.transform.GetChild(nextDeckNum).gameObject.transform.GetChild(0).GetComponent<Image>().sprite;
+
+
+            FormationList.transform.GetChild(preDeckNum).gameObject.GetComponent<FormationDeck>().LinkedChar = 
+            FormationList.transform.GetChild(nextDeckNum).gameObject.GetComponent<FormationDeck>().LinkedChar;
+
+            // 캐릭터 사용중이라는 표시하기.
+            UserDataManager.Inst.formationDic.Add(preDeckNum, nextCharDicKey);
+            charDic[nextCharDicKey].OnFormation = true;
+            charDic[nextCharDicKey].FormationIndex = preDeckNum;
+
+           
+        
+            UserDataManager.Inst.formationDic.Remove(nextDeckNum);
+
+            preDeckNum = UserDataManager.Inst.formationOrderList[index];
+            nextDeckNum = UserDataManager.Inst.formationOrderList[++index];
+            if(nextDeckNum ==2)
+            {
+                UserDataManager.Inst.formationDic.Remove(preDeckNum);
+                FormationList.transform.GetChild(preDeckNum).gameObject.transform.GetChild(0).GetComponent<Image>().sprite = null;
+                FormationList.transform.GetChild(preDeckNum).gameObject.transform.GetChild(1).gameObject.SetActive(true);
+                FormationList.transform.GetChild(nextDeckNum).GetComponent<FormationDeck>().LinkedChar = null;
+
+                //charDic[nextCharDicKey].OnFormation = true;
+                //charDic[nextCharDicKey].FormationIndex = preDeckNum;
+                break;
+            }
+            nextCharDicKey = UserDataManager.Inst.formationDic[nextDeckNum];
+
+        }
+
+
+    }
+
+
     public void ShowEmptyText(bool on)
     {
         if(on)
