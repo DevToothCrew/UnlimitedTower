b3943da508d606d34e2053cbf15bfe79383979b1
kyle <kyle@devtooth.com>
2018-12-10 18:09:20 +0900

    Setting : WebServer Test Environment

---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Network/Test_PacketManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_PacketManager : MonoSingleton<Test_PacketManager>
+{
+    // Use this for initialization
+
+
+    public string _Packet = null;
+
+
+    public void  Test_Receive(string packet)
+    {
+        _Packet = packet;
+    }
+
+    public string GetPakcet()
+    {
+        return _Packet;
+    }
+}
