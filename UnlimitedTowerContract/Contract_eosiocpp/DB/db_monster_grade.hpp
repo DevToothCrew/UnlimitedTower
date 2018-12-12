0f59de086bbe1048a24bf9e7cef02c505bceda8b
canie@devtooth.com <>
2018-12-12 12:15:06 +0900

    db table monster_stat -> object_status 로 통합 변경 db table item_stat -> object_status 로 통합 변경

--- UnlimitedTowerContract/Contract_eosiocpp/DB/db_monster_grade.hpp
+++ UnlimitedTowerContract/Contract_eosiocpp/DB/db_monster_grade.hpp
@@ -2,20 +2,14 @@
 #include "../Common/common_header.hpp"
 
 
-struct monster_stat
-{
-    uint32_t m_str;
-    uint32_t m_dex;
-    uint32_t m_int;
-};
 
 //@abi table cdbmgrade i64
 class cdbmgrade
 {
   public:
     uint64_t m_grade;
-    monster_stat m_min_range;
-    monster_stat m_max_range;
+    object_status m_min_range;
+    object_status m_max_range;
 public:
     uint64_t primary_key() const {return m_grade;}
 };
