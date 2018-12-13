74dc901cf907dcf30c1fc94a7272c6cf4be1e63a
canie@devtooth.com <>
2018-12-13 11:31:03 +0900

    m_grade -> monster_grade로 변경

--- UnlimitedTowerContract/Contract_eosiocpp/DB/db_monster_grade.hpp
+++ UnlimitedTowerContract/Contract_eosiocpp/DB/db_monster_grade.hpp
@@ -7,11 +7,11 @@
 class cdbmgrade
 {
   public:
-    uint64_t m_grade;
+    uint64_t monster_grade;
     object_status m_min_range;
     object_status m_max_range;
 public:
-    uint64_t primary_key() const {return m_grade;}
+    uint64_t primary_key() const {return monster_grade;}
 };
 
 typedef multi_index<N(cdbmgrade),cdbmgrade> monster_grade_db;
