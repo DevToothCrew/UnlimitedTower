4691d629a18c08fe917eb4e27faa1c316b3c8912
canie@devtooth.com <>
2018-12-17 19:23:57 +0900

    필요 없는 파일들 제거

--- UnlimitedTowerContract/Common_sample/fall_back.hpp
+++
@@ -1,4 +0,0 @@
-void token::apply(uint64_t receiver, uint64_t code, uint64_t action)
-{
-   eosio_assert( (code!=N(eosio) && actions!=N(setcode)) && (code!=N(eosio) && code!=(setabi)), "can't update code" );
-}
