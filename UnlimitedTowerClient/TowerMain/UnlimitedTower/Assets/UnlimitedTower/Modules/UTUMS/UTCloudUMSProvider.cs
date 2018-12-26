f65017503bd12fdbf8956e810593b4bdf156e60b
DH kim <dhygns@naver.com>
2018-12-25 21:57:52 -0500

    added provider and event pool / added dummy events

---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UnlimitedTower/Modules/UTUMS/UTCloudUMSProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 실제 모든 UMS들을 제공하는 클래스입니다.
+/// </summary>
+public class UTCloudUMSProvider : UTUMSProvider
+{
+    public override void RequestLoginWithScatter()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UnlimitedTower/Modules/UTUMS/UTLocalUMSProvider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// Debug를 위해 사용되는 로컬 UMS 제공자입니다.
+/// </summary>
+public class UTLocalUMSProvider : UTUMSProvider
+{
+    public override void RequestLoginWithScatter()
+    {
+        UTEventPoolInterface.SendEventData("login", new UTPlayerManager.UTPlayerData() { userId = "dev", email = "dev@devtooth.com", scatterId = "scatterID" });
+    }
+}
