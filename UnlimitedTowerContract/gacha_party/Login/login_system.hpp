01390be2fefe54e87f808edfbc092866cd2c6dfb
canie@devtooth.com <>
2018-12-26 16:56:34 +0900

    getseed 함수 인자값 변경 갓차할때 필요한 이오스 정상화

--- UnlimitedTowerContract/gacha_party/Login/login_system.hpp
+++ UnlimitedTowerContract/gacha_party/Login/login_system.hpp
@@ -30,9 +30,7 @@ class clogin_system
     cdb_system &db_controller;
 
   private:
-    const uint32_t max_charcterslot = 3;
     const uint32_t max_equip_slot = 3;
-    const uint32_t hero_min_status = 1;
     uint32_t hero_total_status = 24;
 
   public:
@@ -75,7 +73,7 @@ class clogin_system
         res.action = transfer_data.memo.substr(0, l_center);
         if(res.action == "gacha")
         {
-            eosio_assert(transfer_data.quantity.amount == 100, "gacha need 1.0000 EOS");
+            eosio_assert(transfer_data.quantity.amount == 10000, "gacha need 1.0000 EOS");
         }
         else if (res.action == "addparty")
         {
@@ -151,7 +149,7 @@ class clogin_system
         eosio_assert(user_iter != auth_user_table.end(), "unknown account");
         eosio_assert(user_iter->hero.state == hero_state::set_status,"free roulette completed status setting");
 
-        uint64_t l_seed = safeseed::get_seed(_user);
+        uint64_t l_seed = safeseed::get_seed(owner, _user);
 
         std::vector<uint64_t> randoms;
         auto &random_value = safeseed::get_total_rand(randoms, hero_total_status);
@@ -189,7 +187,7 @@ class clogin_system
         eosio_assert(user_iter != auth_user_table.end(), "unknown account");
         eosio_assert(user_iter->hero.state == hero_state::set_change_status, "already completed status setting");
 
-        uint64_t l_seed = safeseed::get_seed(_user);
+        uint64_t l_seed = safeseed::get_seed(owner, _user);
 
         std::vector<uint64_t> randoms;
         auto &random_value = safeseed::get_total_rand(randoms, hero_total_status);
