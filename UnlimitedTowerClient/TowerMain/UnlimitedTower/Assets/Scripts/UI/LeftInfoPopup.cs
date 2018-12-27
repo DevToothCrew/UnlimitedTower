ea67323b9a79e44122822dc80b0099d7e2a11a7e
Tom <tom@devtooth.com>
2018-12-27 18:20:49 +0900

    로비 유니티 최신 UI 업데이트중

---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/UI/LeftInfoPopup.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class LeftInfoPopup : MonoBehaviour
+{
+    public Text level;
+    public Text account;
+    public Text eos;
+    public Text gameMoney;
+
+    private void Awake()
+    {
+        SetLeftInfoUserInfoUpdate(UserDataManager.Inst.GetUserInfo());
+    }
+
+    public void SetLeftInfoUserInfoUpdate(UserInfo userInfo)
+    {
+        if (userInfo != null)
+        {
+            int userExp = userInfo.userHero.exp;
+            level.text = "Lv." + Calculator.GetLevelForExp(userExp).ToString();
+            account.text = userInfo.userName;
+
+            eos.text = userInfo.userEOS.ToString();
+            gameMoney.text = userInfo.userMoney.ToString();
+        }
+    }
+
+    public void SetLeftInfoUserLevel(int getExp)
+    {
+        level.text = "Lv." + Calculator.GetLevelForExp(getExp).ToString();
+    }
+
+    public void SetLeftInfoUserEOS(int getEOS)
+    {
+        eos.text = getEOS.ToString();
+    }
+
+    public void SetLeftInfoUserGameMoney(int getGameMoney)
+    {
+        gameMoney.text = getGameMoney.ToString();
+    }
+}
