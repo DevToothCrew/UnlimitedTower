2e4002d1b83bfdf8f262b1b0a4f95c2584cbacf6
DH kim <dhygns@naver.com>
2019-01-02 21:42:55 -0500

    WIP) update gacha images / keep refactoring gacha code

---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UnlimitedTower/Modules/UIInterfaces/Gacha/GachaManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GachaManager : MonoBehaviour {
+
+    static private GachaManager Instance = null;
+
+    private void Awake()
+    {
+        // add event listener to get datas from server
+        // this interface can be changed
+        UTEventPoolInterface.AddEventListener("gacha", (result) =>
+        {
+            if(result is UTFailedData)
+            {
+                Debug.Log("Failed");
+            }
+            else
+            {
+
+            }
+        });
+    }
+
+    private void OnEnable()
+    {
+        Instance = Instance ?? this;
+    }
+
+    private void OnDisable()
+    {
+        Instance = Instance == this ? null : Instance;
+    }
+
+    public void ExecuteGacha(bool withoutAnimation = false)
+    {
+        // Request to server
+        // this interface can be changed
+        UTUMSProvider.Instance.RequestGacha();
+    }
+
+    public void ExitGachaScene()
+    {
+
+    }
+
+}
