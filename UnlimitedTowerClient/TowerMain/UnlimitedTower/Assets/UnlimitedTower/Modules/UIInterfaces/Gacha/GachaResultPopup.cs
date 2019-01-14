a7666372921c7b65d9595ee808b1feaef31a547a
donghyeon kim <dhygns@naver.com>
2019-01-13 20:22:05 -0500

    chagned project version to 2017.4.9f1 / updated gacha scene

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UnlimitedTower/Modules/UIInterfaces/Gacha/GachaResultPopup.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UnlimitedTower/Modules/UIInterfaces/Gacha/GachaResultPopup.cs
@@ -24,69 +24,7 @@ public class GachaResultPopup : MonoBehaviour {
 
     [SerializeField]
     private Text resultIntStat;
-
-    /*
-    public UTObject resultObject
-    { 
-        set
-        {
-            if(resultImage != null)
-            {
-                resultImage.sprite = Resources.Load<Sprite>("UI/CharaterImage/" + value.name);
-            }
-            else
-            {
-                Debug.LogWarning("[GachaResultPopup.resultObject] missing resultImage UI.");
-            }
-
-            if(resultObjectName != null)
-            {
-                resultObjectName.text = value.name;
-            }
-            else
-            {
-                Debug.LogWarning("[GachaResultPopup.resultObject] missing resultObjectName UI.");
-            }
-
-            if (resultExplanation != null)
-            {
-                resultExplanation.text = "empty";
-            }
-            else
-            {
-                Debug.LogWarning("[GachaResultPopup.resultObject] missing resultExplanation UI.");
-            }
-
-            if (value is UTUnit)
-            {
-                var unit = value as UTUnit;
-
-                if(unit.status != null)
-                {
-                    if (resultStrStat != null)
-                    {
-                        resultStrStat.text = "STR  " + unit.status.basicStr.ToString();
-                    }
-
-                    if (resultDexStat != null)
-                    {
-                        resultDexStat.text = "DEX  " + unit.status.basicDex.ToString();
-                    }
-
-                    if (resultIntStat != null)
-                    {
-                        resultIntStat.text = "INT  " + unit.status.basicInt.ToString();
-                    }
-                }
-                else
-                {
-                    Debug.LogWarning("[GachaResultPopup.resultObject] " + unit.name + " has no status by some reason.");
-                }
-            }
-        }
-    }
-    */
-
+    
     private void OnEnable()
     {
         Instance = Instance ?? this;
@@ -105,5 +43,11 @@ public class GachaResultPopup : MonoBehaviour {
     public void Popup()
     {
         gameObject.SetActivateWithAnimation(true);
+    }
+
+    void SetResultObject(object result)
+    {
+        //need to be implemented
+        throw new System.NotImplementedException();
     }
 }
