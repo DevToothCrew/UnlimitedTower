497e135dd48f323c673a392a14a4d4d02b31e5e0
DH kim <dhygns@naver.com>
2019-01-17 22:35:57 -0500

    added post processing / added gacha effect animation (WIP)

---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UnlimitedTower/Modules/UIInterfaces/Gacha/GachaBox Effect/Scripts/GachaBoxParticle.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+public class GachaBoxParticle : MonoBehaviour {
+    [SerializeField]
+    private float maxSpeed;
+
+    [SerializeField]
+    private float maxRadius;
+
+    [SerializeField]
+    private float minRadius;
+
+    [SerializeField]
+    private float targetRadius;
+
+    [SerializeField]
+    private AnimationCurve speedDeltaGraph;
+
+    [SerializeField]
+    private AnimationCurve scaleDeltaGraph;
+
+    private Vector3 preGenDir;
+
+    private float preGenEndDist;
+    private float preGenStartDist;
+    private float preGenDispersionSpeed;
+
+    private Action UpdateAnimation;
+
+    private float dispersionProgress
+    {
+        get
+        {
+            return (transform.localPosition.magnitude - preGenEndDist) / (preGenStartDist - preGenEndDist);
+        }
+    }
+
+    private Vector3 dispersionVelocity
+    {
+        get
+        {
+            return preGenDir * preGenDispersionSpeed;
+        }
+    }
+
+    private float summonProgress
+    {
+        get
+        {
+            return 1.0f - dispersionProgress;
+        }
+    }
+
+    private Vector3 summonVelocity
+    {
+        get
+        {
+            return -preGenDir * maxSpeed * speedDeltaGraph.Evaluate(summonProgress);
+        }
+    }
+
+    private Vector3 scale
+    {
+        get
+        {
+            return Vector3.one * scaleDeltaGraph.Evaluate(summonProgress);
+        }
+    }
+
+    public bool isFinishedSummonAnimation
+    {
+        get
+        {
+            return Mathf.Abs(dispersionProgress) < 0.05f;
+        }
+    }
+
+
+    private void Awake()
+    {
+        //gen pre random val
+        preGenEndDist = Random.Range(0, targetRadius);
+        preGenStartDist = Random.Range(minRadius, maxRadius);
+        preGenDispersionSpeed = maxSpeed * Random.Range(0.1f, 1.0f);
+
+        float rad = Random.Range(0, Mathf.PI * 2.0f);
+        preGenDir = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad), 0.0f);
+
+        //set position
+        transform.localPosition = preGenDir * (preGenStartDist);
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        UpdateAnimation?.Invoke();
+    }
+
+    void UpdateSummonAnimation()
+    {
+        transform.localPosition = transform.localPosition + summonVelocity * Time.deltaTime;
+        transform.localScale = scale;
+    }
+
+    void UpdateDispersionAnimation()
+    {
+        transform.localPosition = transform.localPosition + dispersionVelocity * Time.deltaTime;
+        transform.localScale = scale;
+    }
+
+    public void BeginSummon()
+    {
+        UpdateAnimation = UpdateSummonAnimation;
+    }
+
+    public void BeginDispersion()
+    {
+        UpdateAnimation = UpdateDispersionAnimation;
+    }
+
+}
---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UnlimitedTower/Modules/UIInterfaces/Gacha/GachaBox Effect/Scripts/GachaBoxParticleController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class GachaBoxParticleController : MonoBehaviour
+{
+
+    public GameObject particlePrefab;
+
+
+    //post effect
+    private PostProcessingBehaviour ppb;
+    private BloomModel.Settings bloomSettings;
+
+    //object
+    public GameObject Cube;
+
+    public float maxDelayTime;
+    public int particleCount;
+
+    public float maxPlayTime;
+    public List<GameObject> particles = new List<GameObject>();
+
+    private void Awake()
+    {
+        ppb = GetComponentInChildren<PostProcessingBehaviour>();
+
+        bloomSettings = ppb.profile.bloom.settings;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) BeginSummonAnimation();
+	}
+
+    IEnumerator Translation()
+    {
+        float timer = maxPlayTime;
+
+        Quaternion nextRotation = Quaternion.Euler(20.0f, 0, 0) * Cube.transform.rotation;
+        Vector3 nextPosition = Vector3.down * 0.1f;
+        Vector3 nextScale = Vector3.one * 0.8f;
+
+        while ((timer -= Time.deltaTime) > 0)
+        {
+            Cube.transform.rotation = Quaternion.Lerp(Cube.transform.rotation, nextRotation, Time.deltaTime * 0.2f);
+            Cube.transform.localScale = Vector3.Lerp(Cube.transform.localScale, nextScale, Time.deltaTime * 0.2f);
+            Cube.transform.localPosition = Vector3.Lerp(Cube.transform.localPosition, nextPosition, Time.deltaTime * 0.2f);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator Blooming()
+    {
+        float timer = maxPlayTime;
+        float nextIntensity = 0.0f;
+
+        while ((timer -= Time.deltaTime) > 0)
+        {
+            if (Mathf.Abs(bloomSettings.bloom.intensity - nextIntensity) < 0.05f)
+            {
+                nextIntensity = Mathf.Max(0.0f, bloomSettings.bloom.intensity + Random.Range(-0.8f, 1.2f));
+            }
+
+            bloomSettings.bloom.intensity = Mathf.Lerp(bloomSettings.bloom.intensity, nextIntensity, Time.deltaTime * 16.0f);
+
+            //set dirty
+            ppb.profile.bloom.settings = bloomSettings;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator GenerateParticle(float time)
+    {
+        var go = Instantiate(particlePrefab, transform);
+        particles.Add(go);
+        yield return new WaitForSeconds(time);
+
+        go.GetComponent<GachaBoxParticle>()?.BeginSummon();
+    }
+
+    IEnumerator ConditionChecker()
+    {
+        bool allParticleReady = false;
+
+        while (!allParticleReady)
+        {
+            yield return new WaitForEndOfFrame();
+
+            allParticleReady = true;
+            foreach (var go in particles)
+            {
+                var particle = go.GetComponent<GachaBoxParticle>();
+                if (particle != null)
+                {
+                    allParticleReady = allParticleReady && particle.isFinishedSummonAnimation;
+                }
+            }
+        }
+
+        StopAllCoroutines();
+        BeginDispersionAnimation();
+    }
+
+    public void BeginSummonAnimation()
+    {
+        for (int i = 0; i < particleCount; i++)
+        {
+            var randTime = Random.Range(0, maxDelayTime);
+            StartCoroutine(GenerateParticle(randTime));
+        }
+
+        StartCoroutine(Blooming());
+        StartCoroutine(Translation());
+        StartCoroutine(ConditionChecker());
+    }
+
+    public void BeginDispersionAnimation()
+    {
+        foreach(var go in particles)
+        {
+            var particle = go.GetComponent<GachaBoxParticle>();
+            if (particle != null)
+            {
+                particle.BeginDispersion();
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        bloomSettings.bloom.intensity = 0.0f;
+        ppb.profile.bloom.settings = bloomSettings;
+    }
+}
