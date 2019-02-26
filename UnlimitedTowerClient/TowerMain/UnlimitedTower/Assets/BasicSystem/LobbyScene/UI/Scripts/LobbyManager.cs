d268bb9742e885e0be9c5ea4804d1597ab965b97
Tom <tom@devtooth.com>
2019-02-26 09:53:49 +0900

    Path 변경 및 Lobby Scene 초기화

---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/BasicSystem/LobbyScene/UI/Scripts/LobbyManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+#region Extensions 
+
+static public class LobbyAnimationManager
+{
+    static public void SetActivateWithAnimation(this GameObject uiGO, bool value)
+    {
+        var animator = uiGO.GetComponent<Animator>();
+        if (animator != null)
+        {
+            uiGO.SetActive(true);
+            animator.SetTrigger(value ? "SetVisible" : "SetInvisible");
+        }
+        else
+        {
+            uiGO.SetActive(value);
+        }
+    }
+}
+#endregion
+
+public class LobbyManager : MonoSingleton<LobbyManager> {
+
+    public GameObject CenterUI;
+    public GameObject TopUI;
+    public GameObject LeftUI;
+    public GameObject BottomUI;
+
+    public void Awake()
+    {
+        SCENE_STATE state = UserDataManager.Inst.GetSceneState();
+        if(state == SCENE_STATE.None)
+        {
+            state = SCENE_STATE.Lobby;
+            UserDataManager.Inst.SetSceneState(state);
+        }
+        ChangeSceneState(state);
+    }
+
+    public void ChangeSceneState(SCENE_STATE state)
+    {
+        UserDataManager.Inst.SetSceneState(state);
+
+        switch (state)
+        {
+            case SCENE_STATE.Lobby:
+                LeftUI.SetActivateWithAnimation(true);
+                CenterUI.SetActivateWithAnimation(true);
+                TopUI.SetActivateWithAnimation(true);
+                BottomUI.SetActivateWithAnimation(true);
+                break;
+        }
+    }
+}
--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Resources/UI/PopupUI/PopupUIsManager.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Resources/UI/PopupUI/PopupUIsManager.cs
@@ -46,10 +46,6 @@ public class PopupUIsManager : MonoBehaviour {
         {
             return;
         }
-        
-
-        LobbyManager.Inst.InitCenterPopup();
-        LobbyManager.Inst.CenterPopup.SetActivateWithAnimation(true);
 
         UserServantData servantdata = UserDataManager.Inst.GetHeroInfo();
         heroInfoPopup.ToRegistered(servantdata);
