8141d2949b3a1a78b54f950ade6ac7d2e2d19ac9
Tom <tom@devtooth.com>
2019-03-04 14:30:35 +0900

    Test 삭제

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UnlimitedTowerModule/Modules/UTPlayer/UTPlayerManager.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UnlimitedTowerModule/Modules/UTPlayer/UTPlayerManager.cs
@@ -75,7 +75,7 @@ public class UTPlayerManager : MonoBehaviour {
         public string ToJson()
         {
             //JsonData data = Cheat.Inst.GetBattleActionData(heroTarget, heroAction, monsterTarget, monsterAction);
-            JsonData data = Cheat.Inst.TestGetBattleActionData("devtooth", heroTarget, heroAction, monsterTarget, monsterAction);
+            JsonData data = Cheat.Inst.GetBattleActionData("devtooth", heroTarget, heroAction, monsterTarget, monsterAction);
             return data.ToString();
         }
     }
@@ -89,7 +89,7 @@ public class UTPlayerManager : MonoBehaviour {
         public string ToJson()
         {
             //JsonData data = Cheat.Inst.GetStageStartData(stageNum, partyNum);
-            JsonData data = Cheat.Inst.TestGetStageStartData("devtooth", stageNum, partyNum);
+            JsonData data = Cheat.Inst.GetStageStartData("devtooth", stageNum, partyNum);
             return data.ToString();
         }
     }
