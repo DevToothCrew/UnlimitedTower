763401a18528eb3a0f781f02035be67b3c62ff4c
Tom <tom@devtooth.com>
2019-03-07 12:19:41 +0900

    Login Animation Update

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UISystem/UILogin/LoginUI.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UISystem/UILogin/LoginUI.cs
@@ -1,51 +1,27 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
 public class LoginUI : MonoBehaviour {
-    
-    public Text LoginText;
-    private int state = 0;
 
     public GameObject bannerImage;
     public GameObject loginPopup;
 
 	public void Awake () {
-        LoginText.text = "Scatter Login";
-        state = 0;
         bannerImage.SetActivateWithAnimation(true);
         loginPopup.SetActivateWithAnimation(true);
     }
 
     public void OnClickScatterLogin()
     {
-        LoginText.text = "Scatter Login";
-        state = 0;
+        UTUMSProvider.Instance.RequestLoginWithScatter();
     }
 
     public void OnClickEOSTockLogin()
     {
-        LoginText.text = "EOSTock Login";
-        state = 1;
+        UTUMSProvider.Instance.RequestLoginWithScatter();
     }
 
     public void OnClickETCLogin()
     {
-        LoginText.text = "ETC Login";
-        state = 2;
-    }
-
-    public void OnClickLogin()
-    {
-        switch(state)
-        {
-            case 0:
-            case 1:
-            case 2:
-                UTUMSProvider.Instance.RequestLoginWithScatter();
-                break;
-
-            default:
-                break;
-        }
+        UTUMSProvider.Instance.RequestLoginWithScatter();
     }
 }
