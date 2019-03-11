c0f2725d05c79d7e43786962550234f1e3d6d249
Tom <tom@devtooth.com>
2019-03-11 17:31:52 +0900

    Login Animation Complete

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UISystem/UILogin/LoginUI.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UISystem/UILogin/LoginUI.cs
@@ -4,9 +4,11 @@ public class LoginUI : MonoBehaviour {
 
     public GameObject bannerImage;
     public GameObject loginPopup;
+    public GameObject nodeButton;
 
 	public void Awake () {
         bannerImage.SetActivateWithAnimation(true);
         loginPopup.SetActivateWithAnimation(true);
+        nodeButton.SetActivateWithAnimation(true);
     }
 }
