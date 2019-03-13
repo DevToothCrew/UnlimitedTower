2882361caad69569d21974acd7d46e02279c0c7b
DH kim <dhygns@naver.com>
2019-03-12 21:59:29 -0400

    fixed logic in delayed motion / loading scene and removed duplicated loading fx in lobby scene / added font for loading ui

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/BattleSystem/Script/DelayEffect.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/BattleSystem/Script/DelayEffect.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DelayEffect : MonoBehaviour {
-    public GameObject[] effect = new GameObject[11];
 
-    private void OnEnable()
+    public GameObject[] effect;
+
+    public void Begin()
     {
         StartCoroutine(EffectInstante());
     }
 
+    public void End()
+    {
+        foreach (Transform child in transform)
+            child.GetComponent<MoveGameObject>()?.End();
+
+        StopAllCoroutines();
+    }
+
     IEnumerator EffectInstante()
     {
-        for (; ; )
+        while(true)
         {
-            Instantiate(effect[Random.Range(0, effect.Length)], transform).transform.position = transform.position + new Vector3(Random.Range(-150, 150), Random.Range(-150, 150), 0);
+            int radIdx = Random.Range(0, effect.Length);
+
+            var go = Instantiate(effect[radIdx], transform);
+
+            go.transform.position = 
+                transform.position + 
+                Vector3.right * Random.Range(-150, 150) + 
+                Vector3.up * Random.Range(-150, 150);
+
             yield return new WaitForSeconds(0.2f);
         }
     }
---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UnlimitedTower/Modules/UIInterfaces/LoadingUI/LoadingGageShader.shader
@@ -0,0 +1,60 @@
+﻿Shader "Hidden/LoadingGageShader"
+{
+	Properties
+	{
+		uAlpha("Alpha", range(0.0, 1.0)) = 1.0
+		uProgress ("Progress", range(0.0, 1.0)) = 0.0
+		_MainTex ("Texture", 2D) = "white" {}
+
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+			float uProgress;
+			float uAlpha;
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				fixed2 co = (i.uv - 0.5);
+				float rad = (atan2(-co.x, -co.y) + 3.141592) / (3.141592 * 2.0);
+				rad = smoothstep(uProgress + 0.001, uProgress - 0.001, rad);
+				col.a *= rad * uAlpha;
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
