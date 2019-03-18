112fbcd7eb366f693bd58c54dc9560d34a69cd33
DH kim <dhygns@naver.com>
2019-03-17 20:48:12 -0400

    added button extension

---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UnlimitedTowerModule/Modules/UIInterfaces/Editor/CustomUserInterfaceEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class CustomUserInterfaceEditor
+{
+    [MenuItem("GameObject/UI/Button UI Extension")]
+    static void CreateButtonUIExtension()
+    {
+        Transform parent = Selection.gameObjects?[0]?.transform;
+
+        //creation button gameObject
+        GameObject button = new GameObject("UT UI Button");
+        RectTransform rectTransform = button.AddComponent<RectTransform>();
+        button.AddComponent<CanvasRenderer>();
+        ButtonUIExtension buttonExt = button.AddComponent<ButtonUIExtension>();
+
+        //creation button gameObject
+        GameObject imageGo = new GameObject("Image");
+        Image image = imageGo.AddComponent<Image>();
+        image.transform.SetParent(button.transform, false);
+        buttonExt.targetGraphic = image;
+
+        button.transform.parent = parent;
+        rectTransform.anchoredPosition = Vector2.zero;
+    }
+
+}
