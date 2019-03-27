8d693582dcdbd1e14c0389ef4483ba677bbbaae9
sw <sw@devtooth.com>
2019-03-27 17:09:32 +0900

    Prefab 및 이미지 파일 정리.

---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Lobby/DecontructionSlot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DecontructionSlot : MonoBehaviour {
+    public Image imageCharacter;
+    public Text textLevel;
+    public Text textStr;
+    public Text textDex;
+    public Text textInt;
+
+    public void OnClickSlot()
+    {
+
+    }
+}
---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Lobby/SubViewDeconstruction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubViewDeconstruction : MonoBehaviour {
+    public Text textTitle;
+
+    public DecontructionSlot[] slotList = new DecontructionSlot[9];
+
+    public Text textEos;
+    public Text textUgt;
+
+    public Button buttonGrind;
+    public Button buttonClear;
+
+    void Start()
+    {
+        updateView();
+    }
+
+    void updateView()
+    {
+        textEos.text = string.Format("{0}", 0);
+        textUgt.text = string.Format("{0}", 0);
+    }
+
+    void updateSlot()
+    {
+
+    }
+
+    public void OnClickButtonGrind()
+    {
+
+    }
+
+    public void OnClickButtonClear()
+    {
+
+    }
+
+}
