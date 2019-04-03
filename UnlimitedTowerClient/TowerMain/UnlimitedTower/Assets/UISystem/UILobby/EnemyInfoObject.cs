84d3c9984d6e71bfacbef295641b86367db3faa0
Tom <tom@devtooth.com>
2019-04-03 19:12:17 +0900

    EnemyInfoObject Fix

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UISystem/UILobby/EnemyInfoObject.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/UISystem/UILobby/EnemyInfoObject.cs
@@ -6,12 +6,9 @@ public class EnemyInfoObject : MonoBehaviour {
     public Image GradeImage;
     public Image ResourceImage;
 
-    // 이 데이터를 이용해 뭔가 쓸일이 있을까?
-    private DBStageEnemyData enemyData;
-
     public void SetEnemyImage(int enemyID)
     {
-        enemyData = CSVData.Inst.GetStageEnemyData(enemyID);
+        DBStageEnemyData enemyData = CSVData.Inst.GetStageEnemyData(enemyID);
         if(enemyData == null)
         {
             Debug.LogError("Invalid Enemy Data ID : " + enemyID);
