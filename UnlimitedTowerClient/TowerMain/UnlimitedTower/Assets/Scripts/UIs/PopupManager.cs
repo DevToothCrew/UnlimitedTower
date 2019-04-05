bba105a8046fb029d912cbf6e9cf58707ddb650b
sw <sw@devtooth.com>
2019-04-05 17:08:58 +0900

    각종 메세지 팝업용 화면 구현중.

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/UIs/PopupManager.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/UIs/PopupManager.cs
@@ -1,16 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopupManager : MonoBehaviour {
+    public GameObject FramePopup;
 
-	// Use this for initialization
+    public Button buttonFrame;
+    public Text textTitle;
+    public Text textMessage;
+    public Button buttonCancel;
+    public Button buttonOK;
+
+    enum popup_type
+    {
+        NOTIVE = 0, //안내 타입 팝업 : Cancel/OK 버튼 표시안함, 창 터치로 닫기 가능
+        WARNING,    //경고, 오류 타입 팝업 : Cancel/OK 버튼 표시안함, 창 터치로 닫기 가능
+        CONFIRM     //확인용 팝업 : Cancel, OK 버튼 표시, 창 터치로 닫기 불가
+    };
+    popup_type popupType;
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void updateView()
+    {
+        switch(popupType)
+        {
+            case popup_type.NOTIVE:
+                textTitle.text = "Notice";
+
+                textMessage.text = "";
+
+                buttonFrame.gameObject.SetActive(true);
+                buttonCancel.gameObject.SetActive(false);
+                buttonOK.gameObject.SetActive(false);
+                break;
+            case popup_type.WARNING:
+                textTitle.text = "Warning!";
+
+                textMessage.text = "";
+
+                buttonFrame.gameObject.SetActive(true);
+                buttonCancel.gameObject.SetActive(false);
+                buttonOK.gameObject.SetActive(false);
+                break;
+            case popup_type.CONFIRM:
+                textTitle.text = "Confirm";
+
+                textMessage.text = "";
+
+                buttonFrame.gameObject.SetActive(false);
+                buttonCancel.gameObject.SetActive(true);
+                buttonOK.gameObject.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
 }
