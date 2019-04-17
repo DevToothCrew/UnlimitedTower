93060c3973c595dfe20d0c50edcc0b585f03d3be
Kagura <kagura@devtooth.com>
2019-04-17 15:53:22 +0900

    힐 할때만 MaxHp로 맞춰줌

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/BattleSystem/NewScript/DamageManager.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/BattleSystem/NewScript/DamageManager.cs
@@ -59,6 +59,9 @@ public class DamageManager : MonoSingleton<DamageManager>
                 damageText.text.color = HealColor[0];
                 damageText.outLine.effectColor = HealColor[1];
                 BattleManager.Inst.NowHp[attackInfo.target_position] += attackInfo.damage;
+
+                if (BattleManager.Inst.NowHp[attackInfo.target_position] > BattleManager.Inst.status[attackInfo.target_position].maxHp)
+                    BattleManager.Inst.NowHp[attackInfo.target_position] = BattleManager.Inst.status[attackInfo.target_position].maxHp;
             }
             else
             {
@@ -111,9 +114,6 @@ public class DamageManager : MonoSingleton<DamageManager>
                     }
                 }
                 BattleManager.Inst.NowHp[attackInfo.target_position] -= attackInfo.damage;
-                
-                if (BattleManager.Inst.NowHp[attackInfo.target_position] > BattleManager.Inst.status[attackInfo.target_position].maxHp)
-                    BattleManager.Inst.NowHp[attackInfo.target_position] = BattleManager.Inst.status[attackInfo.target_position].maxHp;
             }
         }
         else
