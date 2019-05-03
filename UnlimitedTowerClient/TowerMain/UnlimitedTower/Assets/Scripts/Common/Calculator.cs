159a26deeb848ebbb3d2b2449d8279cf6763b21d
Kagura <kagura@devtooth.com>
2019-05-03 11:20:10 +0900

    스탯 계산식 변경

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Common/Calculator.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Common/Calculator.cs
@@ -7,7 +7,7 @@ public class Calculator : MonoBehaviour
 
     public static int GetPower(Status status, int level = 1)
     {
-        int attack = (int)( ((status.basicStr + status.basicDex) * 10) * (level / (float)DEFINE.MAX_LEVEL) );
+        int attack = (int)(((status.basicStr + status.basicDex) * 10) * (level / (float)DEFINE.MAX_LEVEL));
         int magic_attack = (int)((status.basicInt * 10.0f) * (level / (float)DEFINE.MAX_LEVEL));
 
         return (attack + magic_attack);
@@ -15,27 +15,27 @@ public class Calculator : MonoBehaviour
 
     public static int GetMaxHp(Status status, int level = 1)
     {
-        return (int)((status.basicStr * 88.0f) * (level / (float)DEFINE.MAX_LEVEL));
+        return (int)((status.basicStr * 66 * ((float)level / DEFINE.MAX_LEVEL)) + status.basicStr * 22);
     }
 
     public static int GetAttack(Status status, int level = 1)
     {
-        return (int)(((status.basicStr + status.basicDex) * 10.0f) * (level / (float)DEFINE.MAX_LEVEL));
+        return (int)(((status.basicStr + status.basicDex) * 8 * ((float)level / DEFINE.MAX_LEVEL)) + (status.basicStr + status.basicDex) * 2);
     }
 
     public static int GetMagicAttack(Status status, int level = 1)
     {
-        return (int)((status.basicInt * 10.0f) * (level / (float)DEFINE.MAX_LEVEL));
+        return (int)((status.basicInt * 8 * ((float)level / DEFINE.MAX_LEVEL)) + status.basicInt * 2);
     }
 
     public static int GetDefence(Status status, int level = 1)
     {
-        return (int)(((status.basicStr + status.basicDex) * 5.0f) * (level / (float)DEFINE.MAX_LEVEL));
+        return (int)(((status.basicStr + status.basicDex) * 4 * ((float)level / DEFINE.MAX_LEVEL)) + (status.basicStr + status.basicDex) * 1);
     }
 
     public static int GetMagicDefence(Status status, int level = 1)
     {
-        return (int)((status.basicInt * 5.0f) * (level / (float)DEFINE.MAX_LEVEL));
+        return (int)((status.basicInt * 4 * ((float)level / DEFINE.MAX_LEVEL)) + status.basicInt * 1);
     }
 
     public static int GetCriticalPercent(Status status, int level = 1)
