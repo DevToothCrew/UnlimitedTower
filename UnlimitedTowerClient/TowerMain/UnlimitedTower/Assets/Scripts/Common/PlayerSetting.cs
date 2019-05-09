fb30f201bfb4b788f65d32387f2405c4e9d99258
Kagura <kagura@devtooth.com>
2019-05-09 15:29:20 +0900

    사운드 뮤트 토글버튼 추가

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Common/PlayerSetting.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Common/PlayerSetting.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerSetting : MonoSingleton<PlayerSetting> {
+public class PlayerSetting : MonoSingleton<PlayerSetting>
+{
     public float volum = 1.0f;
+    public bool volumMute = false;
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
         SceneManager.sceneLoaded += SceneLoadStart;
     }
-    
+
     private void SceneLoadStart(Scene scene, LoadSceneMode mode)
     {
         AudioListener.volume = volum;
@@ -20,6 +22,27 @@ public class PlayerSetting : MonoSingleton<PlayerSetting> {
     public void SoundSlider(float volum)
     {
         this.volum = volum;
-        AudioListener.volume = volum;
+
+        if (volumMute)
+        {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            AudioListener.volume = volum;
+        }
+    }
+
+    public void SoundToggle(bool isMute)
+    {
+        volumMute = isMute;
+        if (volumMute)
+        {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            AudioListener.volume = volum;
+        }
     }
 }
--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Lobby/Setting.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Lobby/Setting.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Setting : MonoBehaviour {
-    public GameObject settingBox; 
     public Slider volumSlider;
+    public Toggle volumToggle;
 
     public void VolumChange()
     {
         PlayerSetting.Inst.SoundSlider(volumSlider.value);
     }
 
+    public void VolumMute()
+    {
+        PlayerSetting.Inst.SoundToggle(volumToggle.isOn);
+    }
+
     public void OnEnable()
     {
         volumSlider.value = PlayerSetting.Inst.volum;
+        volumToggle.isOn = PlayerSetting.Inst.volumMute;
     }
 }
