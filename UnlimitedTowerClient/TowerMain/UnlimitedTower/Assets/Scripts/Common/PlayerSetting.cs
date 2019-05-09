7cddd25f36640e083ab5686c9c2fd32ebb18499a
Kagura <kagura@devtooth.com>
2019-05-09 15:10:02 +0900

    사운드 셋팅 슬라이더 추가

---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Common/PlayerSetting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerSetting : MonoSingleton<PlayerSetting> {
+    public float volum = 1.0f;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += SceneLoadStart;
+    }
+    
+    private void SceneLoadStart(Scene scene, LoadSceneMode mode)
+    {
+        AudioListener.volume = volum;
+    }
+
+    public void SoundSlider(float volum)
+    {
+        this.volum = volum;
+        AudioListener.volume = volum;
+    }
+}
---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Lobby/Setting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Setting : MonoBehaviour {
+    public GameObject settingBox; 
+    public Slider volumSlider;
+
+    public void VolumChange()
+    {
+        PlayerSetting.Inst.SoundSlider(volumSlider.value);
+    }
+
+    public void OnEnable()
+    {
+        volumSlider.value = PlayerSetting.Inst.volum;
+    }
+}
