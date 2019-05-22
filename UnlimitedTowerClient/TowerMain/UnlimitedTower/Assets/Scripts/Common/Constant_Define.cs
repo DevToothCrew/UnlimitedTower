912fad6cac1abcfac17016791e4763d264ae5248
Tom <tom@devtooth.com>
2019-05-22 14:31:36 +0900

    최종버전빌드

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Common/Constant_Define.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Common/Constant_Define.cs
@@ -5,8 +5,8 @@
 //공통 사항을 모아둔 클래스.
 public class DEFINE
 {
-    public static readonly ulong NeedGachaEOS = 1;
-    public static readonly ulong NeedGachaUTG = 10000;
+    public static readonly ulong NeedGachaEOS = 10000;
+    public static readonly ulong NeedGachaUTG = 10000000;
     public static readonly int MonsterMinFormationNum = 5;
     public static readonly int MonsterMaxFormationNum = 9;
     public static readonly int ServantMinFormationNum = 0;
