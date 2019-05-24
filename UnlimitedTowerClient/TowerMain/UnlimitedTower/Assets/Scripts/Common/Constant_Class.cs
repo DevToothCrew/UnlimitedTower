5f6b611c439843f1c75352f7f1a49e06db0cf706
Kagura <kagura@devtooth.com>
2019-05-24 10:51:07 +0900

    장비 적용 버그 수정

--- UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Common/Constant_Class.cs
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Common/Constant_Class.cs
@@ -417,13 +417,13 @@ public class BattleStatus
                     switch (equipmentData.optionType)
                     {
                         case EQUIPMENT_OPTION_TYPE.STR:
-                            Status[EFFECT_ID.STR] = (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
+                            Status[EFFECT_ID.STR] += (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
                             break;
                         case EQUIPMENT_OPTION_TYPE.DEX:
-                            Status[EFFECT_ID.DEX] = (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
+                            Status[EFFECT_ID.DEX] += (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
                             break;
                         case EQUIPMENT_OPTION_TYPE.INT:
-                            Status[EFFECT_ID.INT] = (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
+                            Status[EFFECT_ID.INT] += (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
                             break;
                     }
                 }
@@ -432,16 +432,16 @@ public class BattleStatus
                     switch (equipmentData.optionType)
                     {
                         case EQUIPMENT_OPTION_TYPE.ATK:
-                            Status[EFFECT_ID.ATK] = (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
+                            Status[EFFECT_ID.ATK] += (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
                             break;
                         case EQUIPMENT_OPTION_TYPE.MATK:
-                            Status[EFFECT_ID.MATK] = (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
+                            Status[EFFECT_ID.MATK] += (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
                             break;
                         case EQUIPMENT_OPTION_TYPE.DEF:
-                            Status[EFFECT_ID.DEF] = (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
+                            Status[EFFECT_ID.DEF] += (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
                             break;
                         case EQUIPMENT_OPTION_TYPE.MDEF:
-                            Status[EFFECT_ID.MDEF] = (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
+                            Status[EFFECT_ID.MDEF] += (int)(equipmentData.value * (CSVData.Inst.GetEquipmentUpStatFloat(equipmentData.upgrade, equipmentData.gradeType)));
                             break;
                     }
                 }
