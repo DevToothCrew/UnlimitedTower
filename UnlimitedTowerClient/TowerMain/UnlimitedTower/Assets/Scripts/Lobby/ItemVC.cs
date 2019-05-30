35da668d0972bbaea9a8c5a25a2bb65f1640ebc7
sw <sw@devtooth.com>
2019-05-30 15:33:18 +0900

    아이템 상세정보화면 분리 작업.(NewLobby씬으로 따로 저장)

---
+++ UnlimitedTowerClient/TowerMain/UnlimitedTower/Assets/Scripts/Lobby/ItemVC.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemVC : MonoSingleton<ItemVC>
+{
+    //SubView
+    public GameObject FrameSubView;
+
+    //Info
+    public GameObject FrameItemInfo;
+
+
+    //Menu Buttons UI
+    public Text textButtonBack;
+    public Button[] buttonMenu = new Button[4];
+
+    void Start()
+    {
+        updateAllView();
+    }
+
+    //화면 전체 Update (메뉴버튼, 상세정보창, 스크롤 등)
+    void updateAllView()
+    {
+        FrameItemInfo.SetActive(true);
+    }
+}
